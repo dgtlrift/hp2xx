b97a3deff32806fc92b2b9fd83a38488a975492e
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2003-07-23 00:00:00 -0400

    Import of release hp2xx-3.5.0_a02.tar.gz 3.5.0a2: Fixes by Eugene Doudine: i had misapplied part of his original patch, thereby breaking IW support. My 'arc through three points' code was appropriate only for the trivial case in hp-tests, and  produced strange results and arithmetic errors in real-world cases. The 'PDPU dot' hack has been separated from the line drawing function to allow calling from other HPGL processing functions. As a consequence, more HPGL commands now correctly draw dots when special input conditions are met. test case 'pages.hp' was missing a SP command.

--- sources/hp2xx.h
+++ sources/hp2xx.h
@@ -65,8 +65,8 @@ copies.
  ** 95/03/23  V 3.01  E.B  gnuplot ascii format added
  **/
 
-#define VERS_NO        "3.5.0a1"
-#define VERS_DATE      "03/06/22"
+#define VERS_NO        "3.5.0a2"
+#define VERS_DATE      "03/07/24"
 #define VERS_COPYRIGHT "(c) 1991 - 1994 (V3.20) Heinz W. Werntges"
 #if defined(AMIGA)
 #define VERS_ADDITIONS "\tAmiga additions (V 2.00) by Claus Langhans (92/12/16)\n"
